6652107e7628297c56cdea23e4dbc7efa5075f14
zyfgg-666 <zyfgg-666@users.noreply.github.com>
2026-06-16 09:51:35 +0800

    style: 详情页按钮Button→TextView，青绿圆角(加购)+红色圆角(购买)

--- app/src/main/java/com/example/newbee2/DetailActivity.java
+++ app/src/main/java/com/example/newbee2/DetailActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
     private static final String TAG = "DetailActivity";
     private ImageView ivBack, ivGoods, ivCart;
     private TextView tvName, tvIntro, tvPrice, tvDetail, tvCartBadge;
-    private Button btnAddCart, btnBuy;
+    private TextView btnAddCart, btnBuy;
     private Long goodsId;
     private Goods currentGoods;
 
